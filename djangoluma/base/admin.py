d48353e17ee6bf7270d0eb288b484e645c985604
Luma Rodrigues <rdsluma@gmail.com>
2021-02-15 14:08:31 -0300

    Ajuste de dependências e qualidade de código

--- djangoluma/base/admin.py
+++ djangoluma/base/admin.py
@@ -24,7 +24,6 @@ csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
 
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     add_form_template = 'admin/auth/user/add_form.html'
